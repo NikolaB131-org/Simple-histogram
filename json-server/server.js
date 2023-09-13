90d74a123612c9b0583512170bf33db8c7d3efbb
Nikolai <Nikolai.Balantsev@yandex.ru>
2023-09-13 18:18:52 +0300

    Add /api route to server

--- json-server/server.js
+++ json-server/server.js
@@ -4,5 +4,10 @@ const router = jsonServer.router('json-server/db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.get('/api', (req, res) => {
+  res.jsonp(req.query);
+});
+
 server.use(router);
 server.listen(process.env.PORT);
